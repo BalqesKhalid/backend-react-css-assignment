e6de2c1c3efe30f7c7ea4036b9d18a8cb949f1eb
Balqes al-doulat <55205516+BalqesKhalid@users.noreply.github.com>
2022-03-22 16:31:25 +0300

    update design

--- css-app/src/App.jsx
+++ css-app/src/App.jsx
@@ -19,55 +19,59 @@ function App() {
       <header>
         <AppHeader />
       </header>
-      <div className='roadmap_root'>
-        <Root_course />
-      </div>
+      <center>
+        <div className='roadmap_root'>
+          <Root_course />
+        </div>
 
-      <div className='down_arrow'>
-        <img src={down_arrow} alt='down arrow' />
-      </div>
-      {/*Level 1*/}
-      <div className='level1'>
-        <Java_script_101 />
-        <div className='left_arrow'>
-          <img src={left_arrow} alt='down arrow' />
+        <div className='down_arrow'>
+          <img src={down_arrow} alt='down arrow' />
         </div>
-        <Sql_script_101 />
-      </div>
-      {/*arrow 1*/}
-      <div className='down_arrow'>
-        <img src={down_arrow} alt='down arrow' />
-      </div>
-      {/*Level 2*/}
-      <div className='level2'>
-        <Nodejs_script_201 />
-      </div>
-      {/*arrow 1*/}
-      <div className='down_arrow'>
-        <img src={down_arrow} alt='down arrow' />
-      </div>
-      {/*Level 3*/}
-      <div className='level3'>
-        <Grapgql_script_302 />
-      </div>
-      <div className='additionals'>
-        <div className='additionals_part1'>
-          <img className='image1' src={sub_break_line} alt='break line' />
-          <p className='text'>Additional Courses</p>
-          <img className='image2' src={sub_break_line} alt='break line' />
-        </div>
-        <div className='additionals_part2'>
-          <Agile_course />
-          <Git_course />
-          <Jira_course />
-        </div>
-      </div>
-      <br />
-      <br />
-      <div className='footer'>
-        <Footer />
-      </div>
 
+        {/*Level 1*/}
+
+        <div className='level1'>
+          <Java_script_101 />
+          <div className='left_arrow'>
+            <img src={left_arrow} alt='down arrow' />
+          </div>
+          <Sql_script_101 />
+        </div>
+
+        {/*arrow 1*/}
+        <div className='down_arrow'>
+          <img src={down_arrow} alt='down arrow' />
+        </div>
+        {/*Level 2*/}
+        <div className='level2'>
+          <Nodejs_script_201 />
+        </div>
+        {/*arrow 1*/}
+        <div className='down_arrow'>
+          <img src={down_arrow} alt='down arrow' />
+        </div>
+        {/*Level 3*/}
+        <div className='level3'>
+          <Grapgql_script_302 />
+        </div>
+        <div className='additionals'>
+          <div className='additionals_part1'>
+            <img className='image1' src={sub_break_line} alt='break line' />
+            <p className='text'>Additional Courses</p>
+            <img className='image2' src={sub_break_line} alt='break line' />
+          </div>
+          <div className='additionals_part2'>
+            <Agile_course />
+            <Git_course />
+            <Jira_course />
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className='footer'>
+          <Footer />
+        </div>
+      </center>
     </body>
   );
 }
