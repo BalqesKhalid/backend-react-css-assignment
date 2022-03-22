8d10212baba4c93cb04f47a6b7f18e80838f836b
Balqes al-doulat <55205516+BalqesKhalid@users.noreply.github.com>
2022-03-22 16:54:56 +0300

    updating green box

--- css-app/src/js/core_cources/Grapgql_script_302.jsx
+++ css-app/src/js/core_cources/Grapgql_script_302.jsx
@@ -11,17 +11,18 @@ class Grapgql_script_302 extends Component {
                 <div className='first_row'>
                     <div>
                         <img className='image' src={logo} alt="graphQl logo" />
-                        <div className='duration'>
-                            <p className='hours'>40 Hours</p>
-                            <p className='days'>(6 Days)</p>
-                        </div>
+                     
                         <div className='title'>
                             <p className='first'>GraphQL</p>
                             <p className='last'>JS 302</p>
                         </div>
+                        <div className='duration'>
+                            <p className='hours'>40 Hours</p>
+                            <p className='days'>(6 Days)</p>
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div className='second_row'>
                     <p className='full_course_name_part1'>Full course name</p>
                     <p className='full_course_name_part2'>Practical GraphQL: Become a <br />GraphQL Ninja</p>
                 </div>
--- css-app/src/js/core_cources/Nodejs_script_201.jsx
+++ css-app/src/js/core_cources/Nodejs_script_201.jsx
@@ -11,17 +11,18 @@ class Nodejs_script_201 extends Component {
                 <div className='first_row'>
                     <div>
                         <img className='image' src={logo} img="NodeJS logo" />
-                        <div className='duration'>
-                            <p className='hours'>64 Hours</p>
-                            <p className='days'>(10 Days)</p>
-                        </div>
+                       
                         <div className='title'>
                             <p className='first'>NodeJS</p>
                             <p className='last'>JS 201</p>
                         </div>
+                        <div className='duration'>
+                            <p className='hours'>64 Hours</p>
+                            <p className='days'>(10 Days)</p>
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div className='second_row'>
                     <p className='full_course_name_part1'>Full course name</p>
                     <p className='full_course_name_part2'>The complete Node.js Developer <br />Course (3rd Edition)</p>
                 </div>
