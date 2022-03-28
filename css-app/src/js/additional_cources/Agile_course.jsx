354cf8a35ff43382f690f5f6805e3a8d403a813a
Balqes al-doulat <55205516+BalqesKhalid@users.noreply.github.com>
2022-03-28 14:39:47 +0300

    Final update

--- css-app/src/js/additional_cources/Agile_course.jsx
+++ css-app/src/js/additional_cources/Agile_course.jsx
@@ -6,20 +6,15 @@ class Agile_course extends Component {
     render() {
         return (
             <div className='blue_box'>
-                <div className='first_row'>
-                    <img className='image' src={logo} />
-                    <p className='title'>Agile</p>
 
-                </div>
-                <div className='second_row'>
-                    <div className='type'>
-                        <p className='courseType_part1'>Course type </p>
-                        <p className='courseType_part2'>Online Session </p>
-                    </div>
-                    <p className='hours_number'>4<br />Hours</p>
-                </div>
+                <img className='image' src={logo} />
+                <p className='title'>Agile</p>
 
+                <p className='courseType_part1'>Course type </p>
+                <p className='courseType_part2'>Online Session </p>
+                <p className='hours_number'>4<br />Hours</p>
             </div>
+
         );
     }
 }
--- css-app/src/js/additional_cources/Git_course.jsx
+++ css-app/src/js/additional_cources/Git_course.jsx
@@ -5,18 +5,13 @@ class Git_course extends Component {
     render() {
         return (
             <div className='blue_box'>
-                <div className='first_row'>
-                    <img className='image' src={logo} />
-                    <p className='title'>JIRA</p>
 
-                </div>
-                <div className='second_row'>
-                    <div className='type'>
-                        <p className='courseType_part1'>Course type </p>
-                        <p className='courseType_part2'>Online Session </p>
-                    </div>
-                    <p className='hours_number'>4<br />Hours</p>
-                </div>
+                <img className='image' src={logo} />
+                <p className='title'>JIRA</p>
+
+                <p className='courseType_part1'>Course type </p>
+                <p className='courseType_part2'>Online Session </p>
+                <p className='hours_number'>4<br />Hours</p>
 
             </div>
         );
